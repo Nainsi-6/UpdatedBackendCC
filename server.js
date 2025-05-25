53363db6bb23a45fbbce8af43922e6839379ffec
Nainsi <nainsigupta2022@vitbhopal.ac.in>
2025-05-26 00:32:18 +0530

    Resolve merge conflict

--- server.js
+++ server.js
@@ -566,12 +566,21 @@ const io = socketIo(server, {
 });
 
 // Middleware
+const allowedOrigins = ['https://collegeconnectplatform.onrender.com'];
+
 app.use(cors({
-  origin: "*",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
